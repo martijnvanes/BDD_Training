0dcc60c64060f611a5822ea5de69de96b49ab632
Martijn van Es <martijn.vanes@polteq.com>
2023-10-10 18:50:04 +0200

    Cleanup

--- src/test/java/com/bddTraining/practice/stepdefinitions/WebShopSteps.java
+++ src/test/java/com/bddTraining/practice/stepdefinitions/WebShopSteps.java
@@ -3,19 +3,6 @@ package com.bddTraining.practice.stepdefinitions;
 import io.cucumber.java.en.*;
 
 public class WebShopSteps {
-    @Given("I am a user in the webshop")
-        public void iMAUserInTheWebshop() {
-        System.out.println("Blabla");
-    }
-
-    @When("I add item <xyz> to my shopping cart")
-    public void iAddItemXyzToMyShoppingCart() {
-    }
-
-    @Then("I should see a confirmation I've added item <xyz>")
-    public void iShouldSeeAConfirmationIVeAddedItemXyz() {
-    }
-
     @Given("I'm on the VGP Shop homepage")
     public void iMOnTheVGPShopHomepage() {
     }

f4c275322ac5bbbc6e9db544aae5448dacb55b1d
Martijn van Es <martijn.vanes@polteq.com>
2023-10-10 18:36:42 +0200

    Created stepdefinitions

--- src/test/java/com/bddTraining/practice/stepdefinitions/WebShopSteps.java
+++ src/test/java/com/bddTraining/practice/stepdefinitions/WebShopSteps.java
@@ -1,8 +1,6 @@
 package com.bddTraining.practice.stepdefinitions;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
 
 public class WebShopSteps {
     @Given("I am a user in the webshop")
@@ -16,5 +14,29 @@ public class WebShopSteps {
 
     @Then("I should see a confirmation I've added item <xyz>")
     public void iShouldSeeAConfirmationIVeAddedItemXyz() {
+    }
+
+    @Given("I'm on the VGP Shop homepage")
+    public void iMOnTheVGPShopHomepage() {
+    }
+
+    @When("I go to the SHOP")
+    public void iGoToTheSHOP() {
+    }
+
+    @And("I go to the Hoodie with logo detail page")
+    public void iGoToTheHoodieWithLogoDetailPage() {
+    }
+
+    @And("I add the Hoodie with logo to the cart")
+    public void iAddTheHoodieWithLogoToTheCart() {
+    }
+
+    @Then("I should get a confirmation that the product is added to the shopping cart")
+    public void iShouldGetAConfirmationThatTheProductIsAddedToTheShoppingCart() {
+    }
+
+    @But("I should not be taken directly to my shopping cart")
+    public void iShouldNotBeTakenDirectlyToMyShoppingCart() {
     }
 }
